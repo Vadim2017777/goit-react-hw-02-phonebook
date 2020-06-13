d192c3f4b5d79bd98f29b9baa14369a5b2c48f7a
Vadim <svanovivan@gmail.com>
2020-06-13 22:36:38 +0300

    Add form

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 
-import FeedbackOptions from './FeedBackControls/FeedbackOptions';
+import ContactForm from './ContactForm/ContactForm';
+// import { v4 as uuidv4 } from 'uuid';
+// uuidv4();
 
 class App extends Component {
-  static propTypes = {};
-  static defaultProps = {};
-
   state = {
     contacts: [],
     name: '',
   };
 
+  handleNameChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
+    const { contacts } = this.state;
+    const { name } = this.state;
+
     return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-      </>
+      <div>
+        <h1>Phonebook</h1>
+        {/* <ContactForm value={name} onIput={this.handleInputContacts} /> */}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={name} onChange={this.handleInputContacts} />
+          <button type="submit">Добавить телефон</button>
+        </form>
+        <h2>Contacts</h2>
+        {/* <Filter ... />
+      <ContactList ... /> */}
+      </div>
     );
   }
 }
---
+++ src/componets/ContactForm/ContactForm.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// import './ContactForm.module.css';
+
+// const ContactForm = (value, onInput) => {
+//   return (
+//     <form onSubmit={}>
+//       <input type="text" value={value} onChange={onInput} />
+//       <button type="submit">Добавить телефон</button>
+//     </form>
+//   );
+// };
+
+// export default ContactForm;
